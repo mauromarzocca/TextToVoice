d6b485d9cfe1214d717e1f8aa2e45ed02478900b
Mauro Marzocca <57107500+mauromarzocca@users.noreply.github.com>
2024-10-25 20:46:13 +0200

    Update texttovoice.py

--- texttovoice.py
+++ texttovoice.py
@@ -1,14 +1,19 @@
 from gtts import gTTS
 from playsound import playsound
+from datetime import datetime
 
-audio = 'audio.mp3'
+# Genera un nome file basato sulla data e sull'orario corrente
+current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
+audio = f'audio_{current_time}.mp3'
+
+# Impostazione della lingua
 language = 'it'
 
-# Genera il file audio con gTTS
+# Creazione dell'oggetto gTTS e generazione dell'audio
 sp = gTTS(text="Ciao, io sono TextToVoice", lang=language, slow=False)
 sp.save(audio)
 
-# Riproduci l'audio
+# Riproduzione del file audio
 try:
     playsound(audio)
 except Exception as e:
